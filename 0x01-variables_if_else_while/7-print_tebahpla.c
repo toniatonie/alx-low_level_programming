7b800faca9d5317cd038929a3c0ce5227f97d5ac
toniatonie <toniaawino@gmail.com>
2022-07-04 15:18:31 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include<stdio.>
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+char a;
+for (a = '2'; a >= 'a'; a--)
+putchar(a);
+putchar('\n');
+return (0);
+}
