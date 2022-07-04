70d844078d0902e6e58a2f269a94ec6c71135783
toniatonie <toniaawino@gmail.com>
2022-07-04 14:29:48 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always returns 0
+ */
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+if (a == 'q' || a== 'e')
+{
+continue;
+}
+putchar (a);
+return (0);
+}
